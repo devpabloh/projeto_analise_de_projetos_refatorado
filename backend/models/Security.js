8bbd0c1ab8aa26053cdb43f27cae6559939755e6
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-02-22 13:37:01 -0300

    Criando o models da tabela Security

---
+++ backend/models/Security.js
@@ -0,0 +1,15 @@
+import {DataTypes} from "sequelize";
+import sequelize from "../config/database.js"
+import Project from "./Project.js";
+
+const Security = sequelize.define('Security', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}, 
+    measures: {type: DataTypes.STRING},
+    compliance: {type: DataTypes.STRING}, // compliance recebe um valor de um array de strings, exemplo: ['ISO 27001', 'NIST 800-53']
+    tools: {type: DataTypes.STRING}
+})
+
+Security.belongsTo(Project, { foreignKey: 'projectId', as: 'project' });
+Project.hasMany(Security, { foreignKey: 'projectId', as: 'security' });
+
+export default Security;
