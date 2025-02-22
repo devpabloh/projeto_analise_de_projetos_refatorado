7a5324cebcb4d89029b03b2e93b00a24ac65a79f
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-02-22 12:11:40 -0300

    Criando os models de environment e documentation

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,9 @@
 import {Sequalize} from 'sequelize' // importando sequelize que serve para trabalhar com banco de dados 
 
 // aqui estamos criando uma variavel sequalize que vai receber um novo objeto sequalize
-const sequalize = new Sequalize('analiseDeProjetos', 'postgres', '@Tais84671514', {
+const sequelize = new Sequalize('analiseDeProjetos', 'postgres', '@Tais84671514', {
     host: 'localhost', // especificando o host do banco de dados, host é o computador que vai rodar o banco de dados
     dialect: 'postgres' // especificando o banco de dados que vai ser usado
 })
+
+export default sequelize
--- backend/models/Test.js
+++ backend/models/Test.js
@@ -3,5 +3,12 @@ import sequelize from "../config/database.js"; // importando o Sequelize do arqu
 import Project from "./project.js"; // importando o model do projeto, que é o modelo do projeto, nele definimos os campos do projeto, como nome, descrição, fase, data de início, data de entrega, etc. Também definimos as relações entre os projetos e os usuários. Aqui vamos utilizar para definir a relação entre testes e projetos.
 
 const Test = sequelize.define('Test', {
-    id: 
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    testType: {type: DataTypes.STRING}, // tipo do teste
+    tools: {type: DataTypes.STRING}, // ferramentas utilizadas
+    frequency: {type: DataTypes.STRING} // frequência do teste
 })
+
+Test.belongsTo(Project, {foreignKey: 'projectId', as: 'project'}); // belongsTo é um método que define que um teste pertence a um projeto. foreignKey é o nome do campo que vai armazenar o id do projeto. as é o nome do campo que vai armazenar o projeto.
+
+export default Test;
