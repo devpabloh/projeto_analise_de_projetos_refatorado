0b8664d466df7fd39c3e1a47f6f9245de2c3111a
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-02-24 11:03:07 -0300

    Criando componente de registro

---
+++ frontend/src/components/Register/index.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import axios from "axios"
+
+function Register(){
+    const [error, setError] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleRegister = async (event)=>{
+        event.preventDefault({onRegister})
+        try {
+            // Fazendo requisição para o endpoint de registro
+            const response = await axios.post("http://localhost:3000/auth/register", {name,email, password})
+
+            onRegister(response.data)
+        } catch (err) {
+            setError("Erro de registro, tente novamente.", err)
+        }
+    }
+
+
+    return(
+        <div>
+            <h1>Registro</h1>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleRegister}>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={(event)=> setName(event.target.value)}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange = {(event)=> setEmail(event.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange ={(event)=> setPassword(event.target.value)}
+                />
+                <button type="submit">Registrar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register
