3ea89eb2869e09eaf8efd24c0744a0c2368ec216
Dev Pablo H <pablohenriqueadm94@gmail.com>
2025-02-24 16:51:00 -0300

    organizando o css do componente login

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import axios from 'axios'
+import styles from './Login.module.css'
+import logoAti from '../../assets/logoAti.png'
 
 
 function Login({onLogin}){
@@ -11,7 +13,10 @@ function Login({onLogin}){
         event.preventDefault()
         try {
             // faz a requisição para o endpoint de login
-            const response = await axios.post("http://localhost:3000/auth/login",{email, password})
+            const response = await axios.post("http://localhost:3000/analiseDeProjetos/auth/login", {
+                email,
+                password
+            })
 
             // Armazena o token e os dados do usuário, por exemplo, no localStorage ou no estado global
             localStorage.setItem("token", response.data.token)
@@ -27,24 +32,32 @@ function Login({onLogin}){
     }
 
     return(
-        <div>
-            <h1>Login</h1>
-            {error && <p>{error}</p>}
-            <form onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(event)=>setEmail(event.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Senha"
-                    value={password}
-                    onChange={(event)=>setPassword(event.target.value)}
-                />
-                <button type="submit">Entrar</button>
-            </form>
+        <div className={styles.containerGeral}>
+            
+            <div className={styles.containerImagem}>
+                <img src={logoAti} alt="" />
+            </div>
+            <div className={styles.containerBackgroundLogin}>
+                <div className={styles.containerLogin}>
+                    <h2>Login</h2>
+                    {error && <p>{error}</p>}
+                    <form className={styles.containerForm} onSubmit={handleLogin}>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(event)=>setEmail(event.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            value={password}
+                            onChange={(event)=>setPassword(event.target.value)}
+                        />
+                        <button type="submit">Entrar</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
