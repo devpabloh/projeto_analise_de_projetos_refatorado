af1ba744dfe10637fcd18a27020e9e33e224dcb5
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-02-24 11:03:24 -0300

    importando os componentes de Login e dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
+import { useEffect, useState } from "react"
+import Login from "./components/Login"
+import Dashboard from './components/Dashboard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null)
+
+  useEffect(()=>{
+    // verifica se o usuario esta logado, através dos dados armazenados no localStorage
+    const storedUser = localStorage.getItem("user")
+    // Se o usuario estiver logado, armazena os dados no estado
+    if(storedUser){
+      setUser(JSON.parse(storedUser))
+    }
+  },[])
+
+  if(!user){
+    return(
+      <div>
+        <h1>Bem-vindo ao Sistema de Gestão de Projetos</h1>
+        <Login onLogin={setUser} />
+      </div>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Dashboard user={user}/>
   )
 }
 
