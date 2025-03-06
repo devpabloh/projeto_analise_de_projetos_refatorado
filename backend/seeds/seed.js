1628f3b963a7f064ba7c5674b5ce8e82b87b83d0
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-03-06 17:11:04 -0300

    att

--- backend/seeds/seed.js
+++ backend/seeds/seed.js
@@ -1,20 +1,20 @@
 import bcrypt from "bcrypt";
 import sequelize from "../config/database.js";
 import User from "../models/user.js";
-import '../models/associations.js'; // Importa as associações entre os modelos
+import '../models/associations.js'; 
 
 async function seed(){
   try {
-    // Força a recriação de todas as tabelas
+    
     await sequelize.sync({force: true});
 
     const passwordAdmin = await bcrypt.hash('senhaAdmin', 10);
-    const passwordCommon = await bcrypt.hash('senhaComum', 10); // Fixed typo: senhaComun -> senhaComum
+    const passwordCommon = await bcrypt.hash('senhaComum', 10); 
 
     // Cria os usuários iniciais
     await User.bulkCreate([
-      {name: 'administrador', email: "admin@example.com", password: passwordAdmin, role: 'admin'}, // Fixed typo: exemple -> example
-      {name: 'comum', email: "comun@example.com", password: passwordCommon, role: 'common'} // Fixed typo: exemple -> example
+      {name: 'administrador', email: "admin@example.com", password: passwordAdmin, role: 'admin'}, 
+      {name: 'comum', email: "comun@example.com", password: passwordCommon, role: 'common'} 
     ]);
 
     console.log("Seed concluído!");
